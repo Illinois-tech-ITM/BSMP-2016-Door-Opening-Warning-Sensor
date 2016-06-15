347c7f4d67f2ee21cca3ba93dc8923241703e3cf
Guilherme de Castro <gui.decastro@hotmail.com>
2016-06-15 16:35:43 -0500

    Create a sample variable, peak handling and fixed pointer

--- Arduino_Micro_Code/Arduino_Micro_Code.ino
+++ Arduino_Micro_Code/Arduino_Micro_Code.ino
@@ -1,41 +1,15 @@
 #define LEDPIN 13 // Pin where the LED for warning is hooked up
-#define L1_WARNING_DIST 5 // Distance (in m) Threshold to low warning
-#define L2_WARNING_DIST 2.5 // Distance (in m) Threshold to mid warning
-#define L3_WARNING_DIST 1 // Distance (in m) Threshold to max warning
+#define L1_WARNING_DIST 4 // Distance (in m) Threshold to low warning
+#define L2_WARNING_DIST 3 // Distance (in m) Threshold to mid warning
+#define L3_WARNING_DIST 2 // Distance (in m) Threshold to max warning
 #define __time__(d) (d/343.0*2000000.0) // Convert m to us
 #define __dist__(t) (t/2000000.0*343.0) // Convert us to m
 #define TIMER_HIST 250 // Timer histeresys (in ms) for warning level reduction
 #define SIGPIN 5 // Pin where the SIG from sonic rangefinder is plugged in
-
+#define SAMPLES 2 // Peak Handling 
 void (*warningFunc) ();
+int lim = 3;
 
-void setup() {
-  pinMode(LEDPIN, OUTPUT); // Setting warning pin
-  Serial.begin(9600); // Setting Serial port baud rate
-  while (!Serial) {;} // Wait for the serial port be ready
-}
-
-void loop() {
-  int pulseWidth = sonicPing(); // Getting distance from rangefinder
-
-  // Printing in Serial plotter with fixed y-axis
-  Serial.print(__dist__(20000));
-  Serial.print(F("\t"));
-  Serial.print(0);
-  Serial.print(F("\t"));
-  Serial.print(L1_WARNING_DIST);
-  Serial.print(F("\t"));
-  Serial.print(L2_WARNING_DIST);
-  Serial.print(F("\t"));
-  Serial.print(L3_WARNING_DIST);
-  Serial.print(F("\t"));
-  Serial.println(__dist__(pulseWidth));
-
-  warningHandling(pulseWidth);
-  warningFunc();
-
-  delayMicroseconds(200); 
-}
 
 int sonicPing() {
   // Sending 1 to port SIGPIN for 5us
@@ -46,28 +20,80 @@ int sonicPing() {
 
   // Receiving duration of echo pulse (distance in ms from rangefinder)
   pinMode(SIGPIN, INPUT);
+  //Serial.print("Test2 ");
   return pulseIn(SIGPIN, HIGH, 20000);
+
+  
 }
 
 void warningHandling(int pulseDuration) {
+  static int thres = SAMPLES;
+
+ // Serial.println(("Entrou WarningHandling"));
+
 
   if (pulseDuration < __time__(L3_WARNING_DIST)) { // Warning Level 3
-    warningFunc = warningL3;
+    thres--;
+    if (lim != 3) {
+      thres = SAMPLES;
+    }
+    if (thres == 0) {
+      warningFunc = warningL3;
+      Serial.println(("Fez zona 3"));
+      thres = SAMPLES;
+    }
+    lim = 3;
     
-  } else if (pulseDuration < __time__(L2_WARNING_DIST)) { // Warning Level 2
-    warningFunc = warningL2;
     
-  } else if (pulseDuration < __time__(L1_WARNING_DIST)) { // Warning Level 1
-    warningFunc = warningL1;
-
-  } else { // No Warning
-    warningFunc = noWarning;
+  } 
+  
+  else if (pulseDuration < __time__(L2_WARNING_DIST)) { // Warning Level 2
+    thres--;
+    if (lim != 2) {
+      thres = SAMPLES;
+    }
+    if (thres == 0) {
+      warningFunc = warningL2;
+      Serial.println(("Fez zona 2"));
+      thres = SAMPLES;
+    }
+    lim = 2;
+    
+    
+  } 
+  
+  else if (pulseDuration < __time__(L1_WARNING_DIST)) { // Warning Level 1
+    thres--;
+    if (lim != 1) {
+      thres = SAMPLES;
+    }
+    if (thres == 0) {
+      warningFunc = warningL1;
+      Serial.println(("Fez zona 1"));
+      thres = SAMPLES;
+    }
+    lim = 1;
+    
+    
+  } 
+  
+  else { // No Warning
+    thres--;
+    if (lim != 0) {
+      thres = SAMPLES;
+    }
+    if (thres == 0) {
+      warningFunc = noWarning;
+      Serial.println(("Fez zona 0"));
+      thres = SAMPLES;
+    }
+    lim = 0;
+    
     
   }
-
 }
-
 void noWarning() {
+ // Serial.println(("Desligando LED"));
   if (digitalRead(LEDPIN) == HIGH){
     digitalWrite(LEDPIN, LOW);
   }
@@ -75,18 +101,46 @@ void noWarning() {
 
 void warningL1() {
   static byte lvl = 0;
-  analogWrite(LEDPIN, lvl++);  
+ // Serial.println(("Piscando LED devagar"));
+  if (++lvl == 255) {
+    lvl = 0;
+    digitalWrite(LEDPIN, !digitalRead(LEDPIN));
+  }  
 }
 
 void warningL2() {
   static byte lvl = 0;
-  lvl += 15;
-  analogWrite(LEDPIN, lvl);
+ // Serial.println(("Piscando LED medio"));
+  if (++lvl == 100) {
+    lvl = 0;
+    digitalWrite(LEDPIN, !digitalRead(LEDPIN));
+  }
 }
 
 void warningL3() {
-  if (digitalRead(LEDPIN) == LOW){
-    digitalWrite(LEDPIN, HIGH);
+  static byte lvl = 0;
+  //Serial.println(("Piscando o LED rapido"));
+  if (++lvl == 50) {
+    lvl = 0;
+    digitalWrite(LEDPIN, !digitalRead(LEDPIN));
   }
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(LEDPIN, OUTPUT);
+  Serial.begin(9600);
+  while (!Serial) {;}
+  warningFunc = noWarning;
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int pulseWidth = sonicPing();
+  delayMicroseconds(200);
+
+ // Serial.print("Test1 ");
+  warningHandling(pulseWidth);
+  warningFunc();
 }
 
