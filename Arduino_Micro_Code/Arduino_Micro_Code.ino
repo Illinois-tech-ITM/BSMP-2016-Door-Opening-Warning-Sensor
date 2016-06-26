7e6dc48fee358fd8ae86513f41485991f2efd770
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2016-06-25 20:20:13 -0500

    Organizing and commenting
    Commented the code, added compiler directives for a smaller release code
    but still debuggable and organized both the code and the directory.

--- Arduino_Micro_Code/Arduino_Micro_Code.ino
+++ Arduino_Micro_Code/Arduino_Micro_Code.ino
@@ -1,58 +1,80 @@
-/*
-  Filters:
-  * NONE (default): No filtering
-  * MEDIAN: Take the median of a sample
-  * HIGHEST_MODE: Take the mode of a sample. In case more than
-  one mode, the highest one is returned
-  * LOWEST_MODE: Take the mode of a sample. In case more than
-  one mode, the lowest one is returned
-  * BEST: Take the mode of a sample. In case more than one
-  mode is found, the median is returned
-  * SIMPLE: Continue reading until sample_size consecutive readings
-  are issued
-*/
-#include "Maxbotix.h"
-#define SONIC_SENSOR_PIN 8
-#define SOUNDPIN A4
-#define LEDPIN A3
-#define MAXDIST 200
+#include "Maxbotix/Maxbotix.h"
 
-Maxbotix rangeSensorPW(SONIC_SENSOR_PIN, Maxbotix::PW, Maxbotix::LV, Maxbotix::BEST);
+#define SONIC_SENSOR_PIN 8  // Pin where the Sonic Rangefinder Controller is
+#define SOUNDPIN A4         // Pin where the buzzer signal is
+#define LEDPIN A3           // Pin where warning LED is
+#define MAXDIST 200         // Maximum distance to be "too far away from door"
 
+//#define DEBUG
+
+/********************************************
+ *  Functions and Global Vars declaration   *
+ ********************************************/
 float lerp(float a, float b, float i);
 
-void setup()
-{
+Maxbotix rangeSensorPW(SONIC_SENSOR_PIN, Maxbotix::PW, Maxbotix::LV, Maxbotix::BEST);
+boolean buzzing = false; // For controlling the buzzer toggle
+
+
+/********************************
+ *  Arduino-Specific Functions  *
+ ********************************/
+void setup() {
+#ifdef DEBUG
   Serial.begin(9600);
+  while(!Serial);
+#endif
+
   pinMode(SOUNDPIN, OUTPUT);
   pinMode(LEDPIN, OUTPUT);
 }
 
-void loop()
-{
-  float range = rangeSensorPW.getRange();
-  if (range > MAXDIST)
-  {
+void loop() {
+  float range = rangeSensorPW.getRange(); // Distance (in cm) of closest detected object from Sonic Rangefinder
+#ifdef DEBUG
+  Serial.print("Range: ");Serial.print(range);Serial.println("\tcm.");
+#endif
+
+  if (range > MAXDIST) { // Turn off LED and Buzzer if person is too far
     noTone(SOUNDPIN);
     digitalWrite(LEDPIN, LOW);
-  }
-  else 
-  {
-    float freq = lerp(1760, 55, range/MAXDIST);
-    tone(SOUNDPIN, freq);
     
-    float freq2 = lerp(1, 1000, range/MAXDIST);
+  } else { // Turn on otherwise
+    float freq = lerp(1760, 55, range/MAXDIST); // Sound Frequency of Buzzer
+    float freq2 = lerp(1, 1000, range/MAXDIST); // LED Blink Frequency
     static unsigned long t_start = 0;
     unsigned long m = millis();
+  #ifdef DEBUG
+    Serial.print("freq: ");Serial.print(freq);Serial.print("\thz.\t");
+    Serial.print("freq2: ");Serial.print(freq2);Serial.println("\thz.\t");
+    Serial.print("t_start: ");Serial.print(t_start);Serial.print("\tms.\t");
+    Serial.print("m: ");Serial.print(m);Serial.print("\tms.\t");
+    Serial.print("m-t_start: ");Serial.print(m-t_start);Serial.println("\tms.\t");
+    Serial.print("LED is ");Serial.print(digitalRead(LEDPIN)?"On.":"Off.");
+    Serial.print("\tBuzzer is ");Serial.println(buzzing?"On.":"Off.");
+  #endif
     
-    if(m - t_start >= freq2) {
+    if(m - t_start >= freq2) { // Toggling LED and buzzer
       digitalWrite(LEDPIN, !digitalRead(LEDPIN));
       t_start = millis();
-    }
-  }
-}
 
-float lerp(float a, float b, float i)
-{
+      buzzing = !buzzing;
+      if (buzzing) {
+        noTone(SOUNDPIN);
+      } else {
+        tone(SOUNDPIN, freq);
+      }
+    
+    } // End if Toggle
+    
+  } // End if Alarm System Distance handling
+  
+} // End Loop
+
+
+/*******************
+ *  Our Functions  *
+ *******************/
+float lerp(float a, float b, float i) {
   return (1-i)*a + i*b;
 }
