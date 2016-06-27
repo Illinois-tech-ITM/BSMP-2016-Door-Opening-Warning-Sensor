19a6cb669da34db3e96ba54d90d2e73c2a38cfa6
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2016-06-27 15:19:56 -0500

    Changed single LED to NeoPixel LED ring

--- Arduino_Micro_Code/Arduino_Micro_Code.ino
+++ Arduino_Micro_Code/Arduino_Micro_Code.ino
@@ -1,8 +1,10 @@
 #include "Maxbotix/Maxbotix.h"
+#include "NeoPixel/Neopixel.h"
 
-#define SONIC_SENSOR_PIN 8  // Pin where the Sonic Rangefinder Controller is
-#define SOUNDPIN A4         // Pin where the buzzer signal is
-#define LEDPIN A3           // Pin where warning LED is
+#define SONIC_SENSOR_PIN 3  // Pin where the Sonic Rangefinder Controller is
+#define SOUNDPIN A5         // Pin where the buzzer signal is
+#define LEDPIN A0           // Pin where warning NeoPixel ring is
+#define NUMPIXELS 16        // Number of pixels in the NeoPixel ring
 #define MAXDIST 200         // Maximum distance to be "too far away from door"
 
 //#define DEBUG
@@ -11,9 +13,11 @@
  *  Functions and Global Vars declaration   *
  ********************************************/
 float lerp(float a, float b, float i);
+void solid(uint8_t R, uint8_t G, uint8_t B);
 
-Maxbotix rangeSensorPW(SONIC_SENSOR_PIN, Maxbotix::PW, Maxbotix::LV, Maxbotix::BEST);
-boolean buzzing = false; // For controlling the buzzer toggle
+Maxbotix rangeSensorPW(SONIC_SENSOR_PIN, Maxbotix::PW, Maxbotix::LV, Maxbotix::NONE);
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
+boolean warningOn = false; // For controlling the buzzer toggle
 
 
 /********************************
@@ -24,7 +28,7 @@ void setup() {
   Serial.begin(9600);
   while(!Serial);
 #endif
-
+  pixels.begin();
   pinMode(SOUNDPIN, OUTPUT);
   pinMode(LEDPIN, OUTPUT);
 }
@@ -37,33 +41,35 @@ void loop() {
 
   if (range > MAXDIST) { // Turn off LED and Buzzer if person is too far
     noTone(SOUNDPIN);
-    digitalWrite(LEDPIN, LOW);
+    solid(0,0,0);
     
   } else { // Turn on otherwise
-    float freq = lerp(1760, 55, range/MAXDIST); // Sound Frequency of Buzzer
-    float freq2 = lerp(1, 1000, range/MAXDIST); // LED Blink Frequency
+    float freq = 440;//lerp(1760, 55, range/MAXDIST); // Sound Frequency of Buzzer
+    float freq2 = lerp(0, 100, range/MAXDIST); // LED Blink ms delay
     static unsigned long t_start = 0;
     unsigned long m = millis();
   #ifdef DEBUG
     Serial.print("freq: ");Serial.print(freq);Serial.print("\thz.\t");
-    Serial.print("freq2: ");Serial.print(freq2);Serial.println("\thz.\t");
+    Serial.print("LED delay: ");Serial.print(freq2);Serial.println("\tms.\t");
     Serial.print("t_start: ");Serial.print(t_start);Serial.print("\tms.\t");
     Serial.print("m: ");Serial.print(m);Serial.print("\tms.\t");
     Serial.print("m-t_start: ");Serial.print(m-t_start);Serial.println("\tms.\t");
     Serial.print("LED is ");Serial.print(digitalRead(LEDPIN)?"On.":"Off.");
-    Serial.print("\tBuzzer is ");Serial.println(buzzing?"On.":"Off.");
+    Serial.print("\tBuzzer is ");Serial.println(warningOn?"On.":"Off.");
+    Serial.println();
   #endif
     
     if(m - t_start >= freq2) { // Toggling LED and buzzer
-      digitalWrite(LEDPIN, !digitalRead(LEDPIN));
       t_start = millis();
 
-      buzzing = !buzzing;
-      if (buzzing) {
+      if (warningOn) {
         noTone(SOUNDPIN);
+        solid(0,0,0);
       } else {
         tone(SOUNDPIN, freq);
+        solid(255,0,0);
       }
+      warningOn = !warningOn;
     
     } // End if Toggle
     
@@ -78,3 +84,11 @@ void loop() {
 float lerp(float a, float b, float i) {
   return (1-i)*a + i*b;
 }
+
+void solid(uint8_t R, uint8_t G, uint8_t B) {
+  for (int i = 0; i<NUMPIXELS; i++) {
+    pixels.setPixelColor(i, pixels.Color(R,G,B));
+    pixels.show();
+  }
+}
+
