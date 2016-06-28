0c4fbb717a94e61979cf215325be8d2a76a8987a
Gabriel Barbosa <gabrielfbbarbosa@gmail.com>
2016-06-28 16:38:50 -0500

    Primitive noise handling added
    Still needs to be perfected but it's already kind of working

--- Arduino_Micro_Code/Arduino_Micro_Code.ino
+++ Arduino_Micro_Code/Arduino_Micro_Code.ino
@@ -3,11 +3,15 @@
 
 #define SONIC_SENSOR_PIN 3  // Pin where the Sonic Rangefinder Controller is
 #define SOUNDPIN A5         // Pin where the buzzer signal is
+#define SOUNDFREQ 440       // Sound Frequency of Buzzer
 #define LEDPIN A0           // Pin where warning NeoPixel ring is
 #define NUMPIXELS 16        // Number of pixels in the NeoPixel ring
 #define MAXDIST 200         // Maximum distance to be "too far away from door"
+#define MOVTOLERANCE 5      // Tolerance of distance sensor (in cm) to be considered stopped in place (empirical)
+#define SAMPLE_SIZE 50      // Number of samples to analyze to decide if warning should continue ringing
 
 //#define DEBUG
+#define DEBUG_RANGE
 
 /********************************************
  *  Functions and Global Vars declaration   *
@@ -15,42 +19,56 @@
 float lerp(float a, float b, float i);
 void solid(uint8_t R, uint8_t G, uint8_t B);
 
-Maxbotix rangeSensorPW(SONIC_SENSOR_PIN, Maxbotix::PW, Maxbotix::LV, Maxbotix::NONE);
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KHZ800);
+Maxbotix rangeSensorPW(SONIC_SENSOR_PIN, Maxbotix::PW, Maxbotix::LV, Maxbotix::NONE); // Class for using the sonic rangefinder
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, LEDPIN, NEO_GRB + NEO_KHZ800); // Class for controlling LED ring
 boolean warningOn = false; // For controlling the buzzer toggle
-
+byte notApproachingCount = 0, approachingCount = 0;
+float lastRange = 999;
 
 /********************************
  *  Arduino-Specific Functions  *
  ********************************/
 void setup() {
-#ifdef DEBUG
+#if defined(DEBUG) || defined(DEBUG_RANGE)
   Serial.begin(9600);
   while(!Serial);
 #endif
   pixels.begin();
   pinMode(SOUNDPIN, OUTPUT);
-  pinMode(LEDPIN, OUTPUT);
 }
 
 void loop() {
   float range = rangeSensorPW.getRange(); // Distance (in cm) of closest detected object from Sonic Rangefinder
-#ifdef DEBUG
+#if defined(DEBUG) || defined(DEBUG_RANGE)
+  Serial.println("\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n");
   Serial.print("Range: ");Serial.print(range);Serial.println("\tcm.");
+  Serial.print("lastRange: ");Serial.print(lastRange);Serial.println("\tcm.");
+  Serial.print("lastRange-range (");Serial.print(lastRange - range);Serial.print(") < ");Serial.print(MOVTOLERANCE);Serial.println(lastRange-range<MOVTOLERANCE?" = TRUE":" = FALSE");
+  Serial.print("approachingCount = ");Serial.println(approachingCount);
+  Serial.print("notApproachingCount = ");Serial.println(notApproachingCount);
+//  delay(500);
 #endif
 
-  if (range > MAXDIST) { // Turn off LED and Buzzer if person is too far
+  if (lastRange-range < MOVTOLERANCE) {
+    notApproachingCount = notApproachingCount<SAMPLE_SIZE?notApproachingCount+1:SAMPLE_SIZE;
+    approachingCount = 0;
+  } else {
+    if (++approachingCount >= 2){
+      notApproachingCount = 0;
+    }
+  }
+  lastRange = range;
+  
+  if (range > MAXDIST || notApproachingCount >= SAMPLE_SIZE) { // Turn off LED and Buzzer if person is too far
     noTone(SOUNDPIN);
     solid(0,0,0);
     
   } else { // Turn on otherwise
-    float freq = 440;//lerp(1760, 55, range/MAXDIST); // Sound Frequency of Buzzer
-    float freq2 = lerp(0, 100, range/MAXDIST); // LED Blink ms delay
+    float period = lerp(0, 100, range/MAXDIST); // LED Blink ms delay
     static unsigned long t_start = 0;
     unsigned long m = millis();
   #ifdef DEBUG
-    Serial.print("freq: ");Serial.print(freq);Serial.print("\thz.\t");
-    Serial.print("LED delay: ");Serial.print(freq2);Serial.println("\tms.\t");
+    Serial.print("LED delay: ");Serial.print(period);Serial.println("\tms.\t");
     Serial.print("t_start: ");Serial.print(t_start);Serial.print("\tms.\t");
     Serial.print("m: ");Serial.print(m);Serial.print("\tms.\t");
     Serial.print("m-t_start: ");Serial.print(m-t_start);Serial.println("\tms.\t");
@@ -59,14 +77,14 @@ void loop() {
     Serial.println();
   #endif
     
-    if(m - t_start >= freq2) { // Toggling LED and buzzer
+    if(m - t_start >= period) { // Toggling LED and buzzer
       t_start = millis();
 
       if (warningOn) {
         noTone(SOUNDPIN);
         solid(0,0,0);
       } else {
-        tone(SOUNDPIN, freq);
+        tone(SOUNDPIN, SOUNDFREQ);
         solid(255,0,0);
       }
       warningOn = !warningOn;
